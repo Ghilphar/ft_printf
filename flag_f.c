9e0330a0d1dec91b35e1eb96efd177350572d9a1
Felipe GARIBOTTI <fgaribot@e3r3p7.42.fr>
2019-04-03 17:47:57 +0200

    f progress

--- flag_f.c
+++ flag_f.c
@@ -6,14 +6,52 @@
 /*   By: fgaribot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/01 15:02:28 by fgaribot          #+#    #+#             */
-/*   Updated: 2019/04/01 15:29:50 by fgaribot         ###   ########.fr       */
+/*   Updated: 2019/04/03 17:22:59 by fgaribot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	print_float(double n, t_data *data)
+void	print_field(t_data *data)
 {
-	long long	nbr;
+	while (data->field > data->field_2 && data->minus == 0 && data->zero == 0)
+	{
+		ft_putchar(' ');
+		data->field -= 1;
+		data->i += 1;
+	}
+	while (data->field > data->field_2 && data->minus == 0 && data->zero == 1)
+	{
+		ft_putchar('0');
+		data->field -= 1;
+		data->i += 1;
+	}
+	if (data->neg == -1)
+		ft_putchar('-');
+	if (data->neg != 1 && data->plus == 1)
+		ft_putchar('+');
+}
 
+int		ft_exponent(unsigned long long n)
+{
+	int i;
+
+	i = 0;
+	if (n == 0)
+		i = 1;
+	while (n != 0)
+	{
+		n = n / 10;
+		i++;
+	}
+	return (i);
+}
+
+long double	correct_flags(long double n, t_data *data)
+{
+	data->specifier = 'f';
+	if (data->plus == 1)
+		data->space = 0;
+	if (data->minus == 1)
+		data->zero = 0;
 	if (data->precision == -1)
 		data->precision = 6;
 	if (n < 0)
@@ -21,29 +59,124 @@ void	print_float(double n, t_data *data)
 		n = -n;
 		data->neg = -1;
 	}
-	nbr = (long long)n;
-	data->digits = ft_countdigits_base(nbr, 10);
-	data->i += data->digits;
-	while (data->field > (data->digits + 1 + precision) && data->minus == 0 && data->zero == 0)
-	{
-		ft_putchar(' ');
-		data->field -= 1;
-		data->i += 1;
-	}
-	while (data->field > (data->digits + 1 + precision) && data->minus == 0 && data->zero == 1)
-	{
-		ft_putchar('0');
-		data->field -= 1;
-		data->i += 1;
-	}
-	ft_putllnbr(nbr);
+	if (n == 0)
+		data->neg = 0;
+	data->digits = ft_exponent((unsigned long long)n);
+	if (data->precision == 0)
+		data->field_2 = data->digits;
+	if (data->precision > 0)
+		data->field_2 = data->precision + data->digits + 1;
+	if (data->neg == -1 || data->plus == 1)
+		data->field_2 += 1;
+	data->i += data->field_2;
+	return (n);
 }
 
+void		reverse(char *dest, char *p)
+{
+	char c;
 
+	while (dest++ < p--)
+	{
+		c = *p;
+		*p = *q;
+		*q = c;
+	}
+}
+
+char		*round(char *s, char *f)
+{
+	int		co;
+	char	*t = f;
+
+	co = 1;
+	while (t >= s && co == 1)
+	{
+		if (*t == '.')
+			continue;
+		*t += co;
+		if (*t > '9')
+			*t = '0';
+		else
+			co = 0;
+		t--;
+	}
+	if (co)
+	{
+		t = ++f
+		while (t > s)
+		{
+			*t = *(t - 1);
+			*s = '1';
+			t--;
+		}
+	}
+	return (f);
+}
+
+char		*ftoa_integer(long double n, t_data *data, char *dest)
+{
+	long long		integer;
+	int				i;
+	char			p;
+
+	dest = p;
+	i = 0;
+	integer = (long long)n;
+	if (integer)
+	{
+		while (integer && i++ <= data->digits)
+		{
+			*p++ = '0' + integer % 10;
+			integer /= 10;
+		}
+		reverse(dest, p - 1);
+	}
+	else
+		*dest++ = '0';
+//	if (data->precision == 0)
+//		round_up;
+	return (dest);
+}
+
+char		*ftoa_decimal(long double n, t_data *data, char *dest, char *s)
+{
+	int			precision;
+	long double	decimal;
+
+	decimal = n - (long long)n;
+	precision = data->precision;
+	*dest++ = '.';
+	while (precision)
+	{
+		decimal *= 10;
+		*dest++ = '0' + (int)decimal;
+		decimal -= (int)decimal;
+		precision--;
+	}
+	decimal *= 10;
+	if ((int)decimal > 5)
+		dest = inc(s, dest) + 1;
+	return (dest);
+}
 
 va_list	*flag_f(va_list ap, t_data *data)
 {
-	data->specifier = 'f';
-	print_float(va_arg(ap, double), data);
+	long double		n;
+	char			*save;
+	char			*p;
+	char			*s;
+
+	n = va_arg(ap, long double);
+	n = correct_flags(n, *data);
+	print_field(*data);
+	if (!(save = (char *)malloc(sizeof(str) * (data->field_2 + 1))))
+		return (NULL);
+	p = save;
+	s = save;
+	p = ftoa_integer(n, *data, p);
+	if (data->precision != 0)
+		p = ftoa_decimal(n, *data, p, s);
+
 	return (0);
 }
---
+++ test_f.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+
+void reverse(char* p, char* q)
+{
+	char c;
+
+	while (p++ < q--)
+	{
+		c = *p;
+		*p = *q;
+		*q = c;
+	}
+}
+
+char* inc(char* s, char* e)
+{
+	int co = 1;
+	char* t = e;
+
+	while (t >= s)
+	{
+		if (*t == '.')
+			continue;
+		*t += co;
+		if (*t > '9')
+		{
+			*t = '0';
+			co = 1;
+		}
+		else
+		{
+			co = 0;
+			break;
+		}
+		t--;
+	}	
+	if (co)
+	{
+		t = ++e;
+		while (t > s)
+		{
+			*t = *(t - 1);
+			*s = '1';
+			t--;
+		}
+	}
+	return e;
+}
+char* ftoa(char* dest, long double n, int precision, int sign)
+{
+	char *p;
+	long long integer;
+	long double decimal;
+	int save_pres = precision;
+	char *q; 
+
+	integer = (long long)n;
+	decimal = n - integer;
+	p = dest;
+	//parse sign
+	if (sign && n > 0)
+		*p++ = '+';
+	if (n < 0)
+	{
+		*p++ = '-';
+		integer = -integer;
+		decimal = -decimal;
+	}
+	//parse integer
+	if (integer)
+	{
+		q = p;
+		while (integer)
+		{
+			*q++ = '0' + integer % 10;
+			integer /= 10;
+		}
+		reverse(p, q - 1);
+		p = q;
+	}
+	else
+		*p++ ='0';
+	//parse decimal
+	//void decimal(int precision)
+	//{
+	//	if (precision > 0)
+	//		*p++ = '.';
+	//	while (precision)
+	//	
+	//}
+	if(precision > 0)
+	{
+		*p++ = '.';
+
+		while (precision)
+		{
+			decimal *= 10;
+			*p++ = '0' + (int)decimal;
+			decimal -= (int)decimal;
+			precision--;
+		}
+		if ((long long)(decimal + 0.5))
+			p = inc(dest, p - 1) + 1;
+	}
+//	if (((int)n % 2 == 1) && save_pres == 0 && decimal >= 0.5)
+//		p = inc(dest, p - 1) + 1;
+//	if (n < 1 && save_pres == 0 && decimal > 0.5)
+//		p = inc(dest, p - 1) + 1;
+	*p = '\0';
+	return dest;
+}
+
+int main(void) {
+	char s[50];
+	long double n = 99999999.6485;
+
+	sprintf(s, "%.Lf", n);
+	puts(s);
+	puts(ftoa(s, n, 0, 0));
+
+	return 0;
+}
